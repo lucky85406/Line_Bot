da1233cc9f007c0a882d463347f38b61c61c4d20
G79341 <lucky85406@gmail.com>
2024-01-19 13:19:52 +0800

    update

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def handle_message(event):
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
+                messages=[ImageMessage(type="image",originalContentUrl="https://imgur.com/a/Q6bJWDd",previewImageUrl="https://imgur.com/a/Q6bJWDd")]
             )
         )
 
