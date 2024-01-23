a93cf955d5d0dd32908d94a44ffd2b76bed6d86c
G79341 <lucky85406@gmail.com>
2024-01-23 09:59:05 +0800

    update

--- app.py
+++ app.py
@@ -44,6 +44,172 @@ def callback():
     return 'OK'
 
 
+contents = {
+    "type": "bubble",
+    "hero": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+                "type": "text",
+                "text": "FROM",
+                "offsetStart": "10px",
+                "offsetTop": "5px",
+                "color": "#9D9D9D",
+                "weight": "bold"
+            },
+            {
+                "type": "text",
+                "text": "Taoyuan",
+                "offsetTop": "5px",
+                "offsetStart": "20px",
+                "size": "30px"
+            },
+            {
+                "type": "text",
+                "text": "TO",
+                "offsetTop": "5px",
+                "offsetStart": "10px",
+                "color": "#9D9D9D",
+                "weight": "bold"
+            },
+            {
+                "type": "text",
+                "text": "Chayi",
+                "offsetTop": "5px",
+                "offsetStart": "20px",
+                "size": "30px"
+            },
+            {
+                "type": "separator"
+            }
+        ],
+        "spacing": "5px",
+        "backgroundColor": "#D8D8EB",
+        "alignItems": "flex-start"
+    },
+    "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "時程表",
+                        "size": "25px",
+                        "weight": "bold"
+                    }
+                ]
+            },
+            {
+                "type": "box",
+                "layout": "vertical",
+                "margin": "lg",
+                "spacing": "sm",
+                "contents": [
+                    {
+                        "type": "box",
+                        "layout": "baseline",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "第一站"
+                            },
+                            {
+                                "type": "icon",
+                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                "offsetEnd": "20px"
+                            },
+                            {
+                                "type": "text",
+                                "text": "07:00"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                            {
+                                "type": "image",
+                                "url": "https://i.imgur.com/VNeqIZ2.png",
+                                "offsetStart": "45px"
+                            },
+                            {
+                                "type": "text",
+                                "text": "時程：1hr",
+                                "gravity": "center",
+                                "offsetStart": "8px",
+                                "color": "#8E8E8E"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "baseline",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "第二站"
+                            },
+                            {
+                                "type": "icon",
+                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                "offsetEnd": "20px"
+                            },
+                            {
+                                "type": "text",
+                                "text": "08:00"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                            {
+                                "type": "image",
+                                "url": "https://i.imgur.com/VNeqIZ2.png",
+                                "offsetStart": "45px"
+                            },
+                            {
+                                "type": "text",
+                                "text": "時程：1hr",
+                                "gravity": "center",
+                                "offsetStart": "8px",
+                                "color": "#8E8E8E"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "baseline",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "第三站"
+                            },
+                            {
+                                "type": "icon",
+                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                "offsetEnd": "20px"
+                            },
+                            {
+                                "type": "text",
+                                "text": "09:00"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
+
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def function(event):
@@ -56,7 +222,7 @@ def function(event):
                                    ImageSendMessage(original_content_url="https://i.imgur.com/Pw5ZxG1.jpg",
                                                     preview_image_url='https://i.imgur.com/Pw5ZxG1.jpg'))
     elif Ukey() == "時程表":
-        line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text='hi', contents=transit()))
+        line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text='hi', contents=contents))
     else:
         line_bot_api.reply_message(event.reply_token,
                                    TextSendMessage(text="您好～請先點選下方選單中的開始進行葡萄酒選擇喔"))
