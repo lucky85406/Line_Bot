98f197cb15f18fabc0c40d37f3f58e815454efa0
G79341 <lucky85406@gmail.com>
2024-01-23 10:12:55 +0800

    update

--- app.py
+++ app.py
@@ -46,49 +46,8 @@ def callback():
 
 contents = {
     "type": "bubble",
-    "hero": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-            {
-                "type": "text",
-                "text": "FROM",
-                "offsetStart": "10px",
-                "offsetTop": "5px",
-                "color": "#9D9D9D",
-                "weight": "bold"
-            },
-            {
-                "type": "text",
-                "text": "Taoyuan",
-                "offsetTop": "5px",
-                "offsetStart": "20px",
-                "size": "30px"
-            },
-            {
-                "type": "text",
-                "text": "TO",
-                "offsetTop": "5px",
-                "offsetStart": "10px",
-                "color": "#9D9D9D",
-                "weight": "bold"
-            },
-            {
-                "type": "text",
-                "text": "Chayi",
-                "offsetTop": "5px",
-                "offsetStart": "20px",
-                "size": "30px"
-            },
-            {
-                "type": "separator"
-            }
-        ],
-        "spacing": "5px",
-        "backgroundColor": "#D8D8EB",
-        "alignItems": "flex-start"
-    },
-    "body": {
+    "size": "mega",
+    "header": {
         "type": "box",
         "layout": "vertical",
         "contents": [
@@ -98,8 +57,16 @@ contents = {
                 "contents": [
                     {
                         "type": "text",
-                        "text": "時程表",
-                        "size": "25px",
+                        "text": "FROM",
+                        "color": "#ffffff66",
+                        "size": "sm"
+                    },
+                    {
+                        "type": "text",
+                        "text": "Akihabara",
+                        "color": "#ffffff",
+                        "size": "xl",
+                        "flex": 4,
                         "weight": "bold"
                     }
                 ]
@@ -107,103 +74,285 @@ contents = {
             {
                 "type": "box",
                 "layout": "vertical",
-                "margin": "lg",
-                "spacing": "sm",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "TO",
+                        "color": "#ffffff66",
+                        "size": "sm"
+                    },
+                    {
+                        "type": "text",
+                        "text": "Shinjuku",
+                        "color": "#ffffff",
+                        "size": "xl",
+                        "flex": 4,
+                        "weight": "bold"
+                    }
+                ]
+            }
+        ],
+        "paddingAll": "20px",
+        "backgroundColor": "#0367D3",
+        "spacing": "md",
+        "height": "154px",
+        "paddingTop": "22px"
+    },
+    "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+                "type": "text",
+                "text": "Total: 1 hour",
+                "color": "#b7b7b7",
+                "size": "xs"
+            },
+            {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "20:30",
+                        "size": "sm",
+                        "gravity": "center"
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                            {
+                                "type": "filler"
+                            },
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": [],
+                                "cornerRadius": "30px",
+                                "height": "12px",
+                                "width": "12px",
+                                "borderColor": "#EF454D",
+                                "borderWidth": "2px"
+                            },
+                            {
+                                "type": "filler"
+                            }
+                        ],
+                        "flex": 0
+                    },
+                    {
+                        "type": "text",
+                        "text": "Akihabara",
+                        "gravity": "center",
+                        "flex": 4,
+                        "size": "sm"
+                    }
+                ],
+                "spacing": "lg",
+                "cornerRadius": "30px",
+                "margin": "xl"
+            },
+            {
+                "type": "box",
+                "layout": "horizontal",
                 "contents": [
                     {
                         "type": "box",
                         "layout": "baseline",
                         "contents": [
                             {
-                                "type": "text",
-                                "text": "第一站"
-                            },
-                            {
-                                "type": "icon",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                                "offsetEnd": "20px"
-                            },
-                            {
-                                "type": "text",
-                                "text": "07:00"
+                                "type": "filler"
                             }
-                        ]
+                        ],
+                        "flex": 1
                     },
                     {
                         "type": "box",
-                        "layout": "horizontal",
+                        "layout": "vertical",
                         "contents": [
                             {
-                                "type": "image",
-                                "url": "https://i.imgur.com/VNeqIZ2.png",
-                                "offsetStart": "45px"
-                            },
-                            {
-                                "type": "text",
-                                "text": "時程：1hr",
-                                "gravity": "center",
-                                "offsetStart": "8px",
-                                "color": "#8E8E8E"
+                                "type": "box",
+                                "layout": "horizontal",
+                                "contents": [
+                                    {
+                                        "type": "filler"
+                                    },
+                                    {
+                                        "type": "box",
+                                        "layout": "vertical",
+                                        "contents": [],
+                                        "width": "2px",
+                                        "backgroundColor": "#B7B7B7"
+                                    },
+                                    {
+                                        "type": "filler"
+                                    }
+                                ],
+                                "flex": 1
                             }
-                        ]
+                        ],
+                        "width": "12px"
                     },
                     {
-                        "type": "box",
-                        "layout": "baseline",
-                        "contents": [
-                            {
-                                "type": "text",
-                                "text": "第二站"
-                            },
-                            {
-                                "type": "icon",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                                "offsetEnd": "20px"
-                            },
-                            {
-                                "type": "text",
-                                "text": "08:00"
-                            }
-                        ]
-                    },
-                    {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                            {
-                                "type": "image",
-                                "url": "https://i.imgur.com/VNeqIZ2.png",
-                                "offsetStart": "45px"
-                            },
-                            {
-                                "type": "text",
-                                "text": "時程：1hr",
-                                "gravity": "center",
-                                "offsetStart": "8px",
-                                "color": "#8E8E8E"
-                            }
-                        ]
-                    },
-                    {
-                        "type": "box",
-                        "layout": "baseline",
-                        "contents": [
-                            {
-                                "type": "text",
-                                "text": "第三站"
-                            },
-                            {
-                                "type": "icon",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                                "offsetEnd": "20px"
-                            },
-                            {
-                                "type": "text",
-                                "text": "09:00"
-                            }
-                        ]
+                        "type": "text",
+                        "text": "Walk 4min",
+                        "gravity": "center",
+                        "flex": 4,
+                        "size": "xs",
+                        "color": "#8c8c8c"
                     }
-                ]
+                ],
+                "spacing": "lg",
+                "height": "64px"
+            },
+            {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "20:34",
+                                "gravity": "center",
+                                "size": "sm"
+                            }
+                        ],
+                        "flex": 1
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                            {
+                                "type": "filler"
+                            },
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": [],
+                                "cornerRadius": "30px",
+                                "width": "12px",
+                                "height": "12px",
+                                "borderWidth": "2px",
+                                "borderColor": "#6486E3"
+                            },
+                            {
+                                "type": "filler"
+                            }
+                        ],
+                        "flex": 0
+                    },
+                    {
+                        "type": "text",
+                        "text": "Ochanomizu",
+                        "gravity": "center",
+                        "flex": 4,
+                        "size": "sm"
+                    }
+                ],
+                "spacing": "lg",
+                "cornerRadius": "30px"
+            },
+            {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                    {
+                        "type": "box",
+                        "layout": "baseline",
+                        "contents": [
+                            {
+                                "type": "filler"
+                            }
+                        ],
+                        "flex": 1
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                            {
+                                "type": "box",
+                                "layout": "horizontal",
+                                "contents": [
+                                    {
+                                        "type": "filler"
+                                    },
+                                    {
+                                        "type": "box",
+                                        "layout": "vertical",
+                                        "contents": [],
+                                        "width": "2px",
+                                        "backgroundColor": "#6486E3"
+                                    },
+                                    {
+                                        "type": "filler"
+                                    }
+                                ],
+                                "flex": 1
+                            }
+                        ],
+                        "width": "12px"
+                    },
+                    {
+                        "type": "text",
+                        "text": "Metro 1hr",
+                        "gravity": "center",
+                        "flex": 4,
+                        "size": "xs",
+                        "color": "#8c8c8c"
+                    }
+                ],
+                "spacing": "lg",
+                "height": "64px"
+            },
+            {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "20:40",
+                        "gravity": "center",
+                        "size": "sm"
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                            {
+                                "type": "filler"
+                            },
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": [],
+                                "cornerRadius": "30px",
+                                "width": "12px",
+                                "height": "12px",
+                                "borderColor": "#6486E3",
+                                "borderWidth": "2px"
+                            },
+                            {
+                                "type": "filler"
+                            }
+                        ],
+                        "flex": 0
+                    },
+                    {
+                        "type": "text",
+                        "text": "Shinjuku",
+                        "gravity": "center",
+                        "flex": 4,
+                        "size": "sm"
+                    }
+                ],
+                "spacing": "lg",
+                "cornerRadius": "30px"
             }
         ]
     }
