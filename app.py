1c0f9c1b578d7d89fe18a475a5e1cdc888c8127e
G79341 <lucky85406@gmail.com>
2024-01-23 09:35:49 +0800

    update

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, FlexSendMessage
 )
+from Module.flexModule import transit
 
 app = Flask(__name__)
 
@@ -54,6 +55,8 @@ def function(event):
         line_bot_api.reply_message(event.reply_token,
                                    ImageSendMessage(original_content_url="https://i.imgur.com/Pw5ZxG1.jpg",
                                                     preview_image_url='https://i.imgur.com/Pw5ZxG1.jpg'))
+    if Ukey() == "時程表":
+        line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text='hi', contents=transit()))
     else:
         line_bot_api.reply_message(event.reply_token,
                                    TextSendMessage(text="您好～請先點選下方選單中的開始進行葡萄酒選擇喔"))
